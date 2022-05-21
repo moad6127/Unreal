243f5937b8dfccadc3bbafd3a059c2255a9d5354
moad6127 <moad6127@gmail.com>
2022-05-21 18:54:02 +0900

    새로운 프로젝트 다시만들기
    새로만들기
    
    commit
    
    test
    
    asdfasefaseat segad gs
    
    test
    
    test
    
    tsestestsets
    
    test
    
    test
    
    test
    
    무시목록 추가
    
    무시목록 추가로 푸시하기
    
    대용량 파일스토리지 설치
    
    50mb넘는거 제외시키기
    
    push에러 고치기
    
    연습 프로젝트 생성

---
+++ MyStudy/Source/MyStudy/MyStudy.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyStudy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyStudy, "MyStudy" );
---
+++ MyStudy/Source/MyStudy/MyStudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyStudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSTUDY_API AMyStudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
