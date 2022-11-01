9486494a092b3dd62f55b64748dc11c3a76c801b
moad6127 <moad6127@gmail.com>
2022-11-01 19:45:23 +0900

    유데미 강의
    UE_LOG 알아보기

--- Udemy/Shooter/Source/Shooter/ShooterCharacter.cpp
+++ Udemy/Shooter/Source/Shooter/ShooterCharacter.cpp
@@ -16,6 +16,11 @@ void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(LogTemp, Warning, TEXT("BginPlay() Called!"));
+
+	int myInt{ 42 };
+
+	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"),myInt);
 }
 
 // Called every frame
