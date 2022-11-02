aa78bea731473e9cd185d7789c3cb23b23a11717
moad6127 <moad6127@gmail.com>
2022-11-02 21:20:50 +0900

    유데미 강좌
    키 입력시 캐릭터 이동하게 만들기

--- Udemy/Shooter/Source/Shooter/ShooterCharacter.h
+++ Udemy/Shooter/Source/Shooter/ShooterCharacter.h
@@ -19,6 +19,13 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// 앞과 뒤로 움직일때 필요한 함수
+	void MoveForward(float Value);
+
+	//좌우로 움직이기
+	void MoveRight(float Value);
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
