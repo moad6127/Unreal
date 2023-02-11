30ce35066bef9501234cd65d1d2b5cb41c27143c
moad6127 <moad6127@gmail.com>
2023-02-11 15:19:38 +0900

    Feat : 외부 SSD로 로컬 저장소를 옮김
    외부 저장소로 로컬을 변경하고 ADS기능을 잠궈놓음

--- Udemy/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Udemy/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.33.31630
-#include "C:/Unreal/Udemy/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/Shooter/Unreal/Udemy/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
